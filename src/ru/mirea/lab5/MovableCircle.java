ae6e3486e216bce51732a37aad3cd8f5c0a644ee
DariaOso <71928567+DariaOso@users.noreply.github.com>
2020-11-06 00:46:15 +0300

    Initial commit

---
+++ src/ru/mirea/lab5/MovableCircle.java
@@ -0,0 +1,37 @@
+package ru.mirea.lab5;
+
+public class MovableCircle implements Movable {
+    int radius;
+    MovablePoint center;//Speed - скорость
+
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+        this.radius = radius;
+    }
+
+    public void moveUp() {
+        center.moveUp();
+    }
+
+    public void moveDown() {
+        center.moveDown();
+    }
+
+    public void moveRight() {
+        center.moveRight();
+    }
+
+    public void moveLeft() {
+        center.moveLeft();
+    }
+
+    public String toString() {
+        return "MovableCircle{" +
+                "radius=" + radius +
+                ", center=" + center +
+                '}';
+    }
+}
